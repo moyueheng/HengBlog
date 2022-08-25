f3916982dcdf049b24201f83d47d474820bb7685
Heng <moyueheng@126.com>
2022-08-26 00:47:44 +0800

    feat(django后台): ✨ 添加django-jazzmin作为django后台框架

--- HengBlog/urls.py
+++ HengBlog/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
 ]
+
+
+urlpatterns += i18n_patterns(path("admin/", admin.site.urls))
--- TechnicalArchitecture.md
+++ TechnicalArchitecture.md
@@ -1,2 +1,20 @@
 # 管理页面
+2022年8月份
+最终选择 *django-jazzmin*
+## django-grappelli(3.3k)
+七年前的开始的老项目了，有点丑，虽然还有人在更新，
+但是并没有很活跃
 
+## django-jazzmin(1.1k)
+两年前开始的项目，
+爵士风格的一个admin后台，使用bootstrap4+AdminLTE
+社区活跃，最近一次提交在一个月前
+![](https://s2.loli.net/2022/08/26/lNgRF9OoGEdCBaX.png)
+
+## django-hijack(1.1k)（MARK）
+TODO 这不是一个后台的主题，以后可能会用到 
+两年前开始的项目，
+使用 Django Hijack，管理员可以登录并代表其他用户工作，而无需知道他们的凭据。
+## simpleui (2.7k)
+也挺活跃的，但是最近不是很活跃，感觉团队在搞商业化，算不得真正的开源
+所以不是很想用，虽然挺漂亮的
